0ed71e9b1d6f370efbda38ac2f6bef4f2c5c0e88
ASUS <ivana.liman12@gmail.com>
2024-04-16 14:43:33 +0700

    implementation permissions & activity added

---
+++ app/src/main/java/com/jubaya/myarchive/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jubaya.myarchive.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jubaya.myarchive.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
