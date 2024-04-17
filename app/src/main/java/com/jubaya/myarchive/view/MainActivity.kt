6e1daa96d70ebc600e61aadd01f07700877b514d
ASUS <ivana.liman12@gmail.com>
2024-04-17 16:20:42 +0700

    Home & profile modified, menu & navigation added

--- app/src/main/java/com/jubaya/myarchive/view/MainActivity.kt
+++ app/src/main/java/com/jubaya/myarchive/view/MainActivity.kt
@@ -2,18 +2,34 @@ package com.jubaya.myarchive.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.jubaya.myarchive.R
 import com.jubaya.myarchive.databinding.ActivityMainBinding
 import com.jubaya.myarchive.model.Global
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.txtID.text = Global.userid.toString()
+        navController = (supportFragmentManager.findFragmentById(R.id.navHost)
+                as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
+                || super.onSupportNavigateUp()
     }
 }
