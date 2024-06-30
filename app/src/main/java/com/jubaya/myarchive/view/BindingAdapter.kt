1217fec38dfba8b790bff589a98a2f28f3ddb673
J4d3-2 <s160421021@student.ubaya.ac.id>
2024-06-30 20:08:51 +0700

    Custom Photo Binding Added & Profile Modified

---
+++ app/src/main/java/com/jubaya/myarchive/view/BindingAdapter.kt
@@ -0,0 +1,39 @@
+package com.jubaya.myarchive.view
+
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.databinding.BindingAdapter
+import com.jubaya.myarchive.R
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.lang.Exception
+
+@BindingAdapter("android:imageUrl")
+fun loadPhoto(imageView: ImageView, url:String) {
+    val picasso = Picasso.Builder(imageView.context)
+    picasso.listener { picasso, uri, exception ->
+        exception.printStackTrace()
+    }
+
+    if (url.isNullOrEmpty()) {
+        // Set a placeholder or a default image if the URL is null or empty
+        imageView.setImageResource(R.drawable.ic_launcher_foreground)
+    } else {
+        picasso.build().load(url).into(imageView, object: Callback {
+            override fun onSuccess() {
+                var v = imageView.parent as View
+                var pb = v.findViewById<ProgressBar>(R.id.progressImage)
+                pb.visibility = View.INVISIBLE
+                imageView.visibility = View.VISIBLE
+            }
+
+            override fun onError(e: Exception?) {
+                Log.e("picasso", e?.message.toString())
+            }
+        })
+        //picasso.build().load(url).into(imageView)
+    }
+    Log.d("bindingadapter", "cek "+url)
+}
